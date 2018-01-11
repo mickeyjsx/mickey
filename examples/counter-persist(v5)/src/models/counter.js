8fb276399dbc2a737948c7d83eb88ed8fc39f02e
yohacai <yohacai@yohacaideMacBook-Pro.local>
2018-01-11 20:16:17 +0800

    Updated dependency version of React \nAdd redux-persist v5 example

---
+++ examples/counter-persist(v5)/src/models/counter.js
@@ -0,0 +1,23 @@
+import { REHYDRATE } from 'redux-persist'
+
+const delay = timeout => new Promise((resolve) => {
+  setTimeout(resolve, timeout)
+})
+
+export default {
+  namespace: 'counter',
+  state: {
+    count: 0,
+    loading: false,
+  },
+  increment: state => ({ ...state, count: state.count + 1 }),
+  decrement: state => ({ ...state, count: state.count - 1 }),
+  incrementAsync: {
+    * effect(payload, { call }, { succeed }) {
+      yield call(delay, 2000)
+      yield succeed()
+    },
+    prepare: state => ({ ...state, loading: true }),
+    succeed: state => ({ ...state, count: state.count + 1, loading: false }),
+  },
+}
---
+++ examples/counter-persist-immutable(v5)/README.md
@@ -0,0 +1,15 @@
+# Counter-Persist-Immutable(v5)
+
+> Counter example work with [redux-persist(v5)](https://github.com/rt2zz/redux-persist) and [ImmutableJS](https://github.com/facebook/immutable-js/) for mickey
+
+## Features
+
+- Integrate [Redux](https://github.com/rackt/redux) & [ImmutableJs](https://facebook.github.io/immutable-js/)
+- Delay render until rehydration complete
+
+## Usage
+
+```bash
+$ npm install
+$ npm start
+```
